e09947bd75efbe7a5f37543085cf9ce329842448
tmazhuan <thomas.mazhuancherry@gmail.com>
2021-03-01 16:55:22 +0100

    Config extended with Storage

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,13 +1,12 @@
-// pub mod config {
 use serde::{Deserialize, Serialize};
 use toml;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize)]
 pub struct Replace {
     pub from: String,
     pub to: String,
 }
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize)]
 pub struct Config {
     pub symbols: Vec<String>,
     pub regex_expressions: Vec<String>,
@@ -16,39 +15,60 @@ pub struct Config {
     pub what_is_regex: String,
     pub replace: Vec<Replace>,
 }
-
-impl Default for Config {
+pub struct ConfigObject {
+    pub configuration: Config,
+    source: String,
+}
+impl Default for ConfigObject {
     fn default() -> Self {
         //default config file is stored in ./config/config.toml
         let config_default_location = String::from("./config/config.toml");
         let config_text = std::fs::read_to_string(&config_default_location).unwrap();
         let config_const_values: Config = toml::from_str(&config_text).unwrap();
-        config_const_values
+        ConfigObject {
+            configuration: config_const_values,
+            source: config_default_location,
+        }
     }
 }
 
-impl Config {
-    pub fn new(config_file_location: String) -> Config {
+impl ConfigObject {
+    pub fn new(config_file_location: String) -> ConfigObject {
         let config_text = std::fs::read_to_string(&config_file_location).unwrap();
         let config_const_values: Config = toml::from_str(&config_text).unwrap();
-        config_const_values
+        ConfigObject {
+            configuration: config_const_values,
+            source: config_file_location,
+        }
+    }
+
+    pub fn store(&self) -> std::io::Result<()> {
+        std::fs::write(
+            &self.source,
+            toml::to_string(&self.configuration).unwrap().as_bytes(),
+        )
     }
 }
-// }
 
 #[cfg(test)]
 mod tests {
-    use super::Config;
+    use super::ConfigObject;
     #[test]
     fn test_new() {
-        let config_file = Config::new(String::from("./config/test.toml"));
-        assert_eq!(config_file.about_regex, String::from("about_regex"));
-        assert_eq!(config_file.what_is_regex, String::from("what_is_regex"));
-        assert_eq!(config_file.symbols.len(), 3);
+        let config_file = ConfigObject::new(String::from("./config/test.toml"));
+        assert_eq!(
+            ConfigObject.configuration.about_regex,
+            String::from("about_regex")
+        );
+        assert_eq!(
+            ConfigObject.configuration.what_is_regex,
+            String::from("what_is_regex")
+        );
+        assert_eq!(ConfigObject.configuration.symbols.len(), 3);
     }
     #[test]
     fn test_default() {
-        let config_file: Config = Default::default();
+        let config_file: ConfigObject = Default::default();
         assert_eq!(
             config_file.about_regex,
             String::from(r#"<(h2)\s{1}.{1,20}\s{1}(class=".*?")>About.{1,30}</h2>"#)
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,47 @@ pub mod config;
 
 use config::*;
 
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Config {
+    ip: String,
+    port: Option<u16>,
+    keys: Keys,
+}
+
+#[derive(Serialize)]
+struct Keys {
+    github: String,
+    travis: Option<String>,
+}
+
 fn main() {
-    let config_file = Config::new(String::from("./config/config.toml"));
-    println!("{}", config_file.about_regex);
+    let mut config_object = ConfigObject::new(String::from("./config/config.toml"));
+    println!("{}", config_object.configuration.about_regex);
+    config_object.configuration.symbols = vec![
+        String::from("abc"),
+        String::from("def"),
+        String::from("ghi"),
+    ];
+    match config_object.store() {
+        Ok(_) => println!("Configuration Stored"),
+        Err(e) => println!("Configuration not stored. Error: {}", e),
+    }
+    // quick_test();
+}
+
+#[allow(unused)]
+fn quick_test() {
+    let config = Config {
+        ip: "33.33.33.33".to_string(),
+        port: None,
+        keys: Keys {
+            github: "lkasjs".to_string(),
+            travis: Some("sadfa3".to_string()),
+        },
+    };
+    let config_default_location = String::from("./config/xxx.toml");
+    let toml = toml::to_string(&config).unwrap();
+    std::fs::write(config_default_location, toml.as_bytes()).unwrap();
 }
