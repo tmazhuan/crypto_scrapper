a3bf1f9ebb0931819f565ecaddcba3bbe7ab67d2
tmazhuan <thomas.mazhuancherry@gmail.com>
2021-03-03 22:17:37 +0100

    old methods deleted and modules cleaned

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@ pub mod config;
 use config::ConfigObject;
 use html_parser::*;
 use regex::Regex;
-use scraper::{ElementRef, Html, Selector};
 
 pub struct MarketResult {
     source: String,
@@ -47,46 +46,6 @@ impl CoinMarketCapScrapper {
         what_is_inner = self.cleanup_result_string(what_is_inner);
         Ok(what_is_inner)
     }
-    // pub fn get_details(&self, symbol: &str) -> Result<String, ParseError> {
-    //     let url = format!("https://coinmarketcap.com/currencies/{}/", symbol);
-    //     let html = reqwest::blocking::get(&url).unwrap().text().unwrap();
-    //     let document = Html::parse_document(&html);
-    //     let result = self.get_what_is_html_element(&html);
-    //     let (t, v) = match result {
-    //         Ok((t, v)) => (t, v),
-    //         Err(e) => {
-    //             return Err(ParseError::new(format!(
-    //                 "Error ({}) when reading page. Check manually on {}",
-    //                 e, url
-    //             )));
-    //         }
-    //     };
-    //     let selector = Selector::parse(&format!("{}[{}]", t, v)).unwrap();
-    //     let mut element = document.select(&selector).next().unwrap();
-    //     let mut iter = element.next_siblings();
-    //     let mut next_element = false;
-    //     let mut result = String::new();
-    //     while !next_element {
-    //         next_element = match iter.next() {
-    //             Some(e) => {
-    //                 element = ElementRef::wrap(e).unwrap();
-    //                 let l = element.value().name();
-    //                 if l == "h2" || l == "h3" {
-    //                     true
-    //                 } else {
-    //                     // println!("{}", element.inner_html());
-    //                     // clean_text_from_links(element.inner_html());
-    //                     result.push_str(&format!("{}\n", element.inner_html()));
-    //                     // element = ElementRef::wrap(iter.next().unwrap()).unwrap();
-    //                     false
-    //                 }
-    //             }
-    //             None => true,
-    //         };
-    //     }
-    //     // println!("{}", self.cleanup_result_string(result));
-    //     Ok(self.cleanup_result_string(result))
-    // }
 
     fn cleanup_result_string(&self, mut text: String) -> String {
         for expr in &self.cfg.configuration.regex_expressions {
@@ -107,32 +66,6 @@ impl CoinMarketCapScrapper {
             .into_owned();
         String::from(text)
     }
-    // fn get_what_is_html_element(&self, html: &String) -> Result<(String, String), &'static str> {
-    //     //<h2 id="what-is-cryptocom-coin-cro">What Is Crypto.com Coin [CRO]?</h2>
-    //     // let re = Regex::new(r#"<(h2)\s{1}(.{1,10}="what-is-.*?")>.*?</h2>"#).unwrap();
-    //     let re = Regex::new(&self.cfg.configuration.what_is_regex).unwrap();
-    //     // let mut result = String::new();
-    //     let capture = re.captures(&html);
-    //     match capture {
-    //         Some(cap) => {
-    //             let t = &cap[1];
-    //             let v = &cap[2];
-    //             return Ok((String::from(t), String::from(v)));
-    //         }
-    //         None => {
-    //             let re = Regex::new(&self.cfg.configuration.about_regex).unwrap();
-    //             let capture = re.captures(&html);
-    //             match capture {
-    //                 Some(cap) => {
-    //                     let t = &cap[1];
-    //                     let v = &cap[2];
-    //                     return Ok((String::from(t), String::from(v)));
-    //                 }
-    //                 None => return Err("Element not Found"),
-    //             };
-    //         }
-    //     };
-    // }
 
     pub fn get_price(&self, symbol: &str) -> Result<(f64, f64), ParseError> {
         let url = format!("https://coinmarketcap.com/currencies/{}/", symbol);
@@ -183,8 +116,6 @@ impl CoinMarketCapScrapper {
         Ok((price, percentage))
     }
 
-    // pub fn get_markets(&self, symbol: &str) -> Result<MarketResult, Error> {}
-
     #[allow(unused)]
     pub fn get_7d_performance(&self, symbol: &str) -> f64 {
         0.0
@@ -225,11 +156,6 @@ mod html_parser {
         Parent,
         Child(i32),
         Sibling(i32),
-    }
-
-    pub struct HtmlParseResult {
-        pub tag: String,
-        pub attribute: String,
     }
 
     pub fn get_element_and_attribute(
